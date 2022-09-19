e9faec5fcbdd80e5abc3394740f702fa14716211
lml <2084387897@qq.com>
2022-09-19 20:06:35 +0800

    feat(custom):

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    再次初始化2.0
+  <div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data () {
+    return {
+
+    }
   },
-};
+  methods: {
+
+  },
+  components: {
+
+  }
+}
 </script>
+
+<style scoped lang='scss'>
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    porh: 8888,
+    host: "localhost",
+    open: true,
+    https: false,
+  },
   lintOnSave: false, //关闭语法检查
 });
